9bb3e908f785fe3abe9c016ed72f4ca03a0331d0
suhaylaa <suhayla.ali@laetottenham.ac.uk>
2024-10-31 11:09:47 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -3,11 +3,26 @@ import math # you'll probably need this
 exchange_rates = {
     'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
     'EUR': 1.15,
-}
+    'CAD': 1.80,
+    'YUAN': 9.27,
+    'DIRHAM': 4.79
+    }
+yes = input("Would you like to convert to a a different currency? y/n: ").upper()
+if yes == "Y":
+    new_c = input("Pick a currency to convert to : ").upper()
+clist = ['USD', 'EUR', 'CAD','YUAN', 'DIRHAM']
+def check_currency_exists(new_c):
+    while new_c == "USD" or 'CAD' or 'YUAN' or 'DIRHAM' or 'EUR':
+            print("We can convert to that currency!!")
+    else:
+        print("We cannot convert to that currency.")
+        new_c = input("Pick a currency to convert to : ").upper()
 
-def check_currency_exists(currency):
-    return
-
+    return new_c
+new_c = check_currency_exists()
+amount = 37.00
+original_c = 1 
 def currency_convert(original_c, new_c, amount):
-    # your code here
-    return
+    price = amount * exchange_rates[new_c]
+    print(price)
+currency_convert()
